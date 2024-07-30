18415bb269d4d7581ff87f9779221d8f438583a2
Walter-Britos1 <walterbritos418@gmail.com>
2024-07-30 20:38:17 -0300

    styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card({ category, thumb }) {
   return (
-    <div className='bg-white rounded-lg mb-[-10px] shadow-lg overflow-hidden transform transition duration-500 hover:scale-105'>
+    <div className='bg-black bg-opacity-50 rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105'>
       <img src={thumb} alt={category} className='w-full h-48 object-cover' />
       <div className='p-4'>
-        <h2 className='text-lg font-semibold mb-2'>{category}</h2>
+        <h2 className='text-lg text-white font-semibold mb-2'>{category}</h2>
       </div>
     </div>
   );
--- src/components/CatalogSection/CatalogSection.jsx
+++ src/components/CatalogSection/CatalogSection.jsx
@@ -13,9 +13,9 @@ export default function CatalogSection() {
   }
 
   return (
-    <div className='bg-gray-100 mb-[-75px] py-8'>
+    <div className='bg-black bg-opacity-90 mb-[-80px] py-8'>
       <div className='container mx-auto px-4'>
-        <h2 className='text-3xl font-bold mb-6 text-center'>Our Menu</h2>
+        <h2 className='text-3xl font-bold mb-6 text-white text-center font-sans'>Our Menu</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
           {categories.map((item) => (
             <Card
