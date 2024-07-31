436bdb5fa8b216d41c23fca6e7de80b47e09a48b
Walter-Britos1 <walterbritos418@gmail.com>
2024-07-30 21:41:58 -0300

    creating Loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import loadingIcon from '../../assets/loading.gif';
+
+export default function Loading() {
+  return (
+    <div>
+      <img src={loadingIcon} alt='Loading Icon' />
+    </div>
+  )
+}
