9548b277a6c797af736ba25657046ae37a808f09
Walter-Britos1 <walterbritos418@gmail.com>
2024-07-30 21:57:31 -0300

    styling logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../../../public/logo.png';
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className='bg-black bg-opacity-90 text-white py-4'>
       <div className='container mx-auto flex justify-between items-center px-4'>
-        <div className='text-2xl font-bold'>
-          <Link to='/' className='hover:text-gray-400'>
-            Logo
+        <div className='flex items-center'>
+          <Link to='/' className='hover:text-gray-400 flex items-center' onClick={handleLinkClick}>
+            <img 
+              src={logo} 
+              alt='logo' 
+              className='w-16 h-auto md:w-24 mr-2' 
+            />
+            <span className='text-2xl font-bold hidden md:block'>
+              Delicious Food
+            </span>
           </Link>
         </div>
         <div className='hidden md:flex space-x-4'>
@@ -34,13 +46,13 @@ export default function NavBar() {
         className={`md:hidden transition-all duration-300 ease-in-out ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}
       >
         <div className='flex flex-col items-center space-y-4 mt-4'>
-          <Link to='/' className='hover:text-gray-400'>
+          <Link to='/' className='hover:text-gray-400' onClick={handleLinkClick}>
             HOME
           </Link>
-          <Link to='/catalog' className='hover:text-gray-400'>
+          <Link to='/catalog' className='hover:text-gray-400' onClick={handleLinkClick}>
             CATALOG
           </Link>
-          <Link to='/contact' className='hover:text-gray-400'>
+          <Link to='/contact' className='hover:text-gray-400' onClick={handleLinkClick}>
             CONTACT
           </Link>
         </div>
