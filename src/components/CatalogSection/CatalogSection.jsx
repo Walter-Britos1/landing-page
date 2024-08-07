bba63336b0dc5ca50086151bdce661003ca32abb
Walter-Britos1 <walterbritos418@gmail.com>
2024-08-06 22:27:28 -0300

    channge loadin for skeleton component

--- src/components/CatalogSection/CatalogSection.jsx
+++ src/components/CatalogSection/CatalogSection.jsx
@@ -6,9 +6,18 @@ export default function CatalogSection() {
   const { categories, loading, error } = useMeals();
 
   if (loading) {
-    return <div className='bg-black bg-opacity-60 flex justify-center items-center h-screen'>
-      <Loading />
-    </div>;
+    return(
+      <div className='bg-black bg-opacity-90 mb-[-80px] py-8'>
+        <div className='container mx-auto px-4'>
+          <h2 className='text-3xl font-bold mb-6 text-white text-center font-sans'>Our Menu</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <Loading key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
   }
 
   if (error) {
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,10 @@
-import loadingIcon from '../../assets/loading.gif';
-
 export default function Loading() {
   return (
-    <div>
-      <img src={loadingIcon} alt='Loading Icon' />
+    <div className='bg-opacity-0 rounded-lg overflow-hidden transform transition duration-500 hover:scale-105'>
+      <div className='w-full h-48 bg-gray-300 animate-shimmer bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 bg-[length:200%_100%]'></div>
+      <div className='p-4'>
+        <div className='h-6 bg-gray-300 rounded mb-2 w-1/2 animate-shimmer bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 bg-[length:200%_100%]'></div>
+      </div>
     </div>
   )
 }
